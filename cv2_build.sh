bf3ac6ba3cc75ba67e564dbd5174a225cd41e522
bojacthesnake <57487951+bojacthesnake@users.noreply.github.com>
2022-04-04 17:55:30 +0800

    .

--- cv2_build.sh
+++ cv2_build.sh
@@ -63,19 +63,19 @@ install_dependencies () {
     # package repository or should already be installed (eg. CUDA).
     echo "Installing build dependencies."
     # well, shit, they fixed it, so we do this to get the certs temporarily
-    apt-get install -y wget gnupg
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/sbsa/cuda-ubuntu1804.pin 
-    mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_arm64.deb
-    apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
-    dpkg -i cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_arm64.deb
+    # apt-get install -y wget gnupg
+    # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/sbsa/cuda-ubuntu1804.pin 
+    # mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    # wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_arm64.deb
+    # apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
+    # dpkg -i cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_arm64.deb
     
     apt-get update && apt-get install -y --no-install-recommends \
         gosu \
-        cuda-compiler-10-2 \
-        cuda-minimal-build-10-2 \
-        cuda-libraries-dev-10-2 \
-        libcudnn8-dev \
+        # cuda-compiler-10-2 \
+        # cuda-minimal-build-10-2 \
+        # cuda-libraries-dev-10-2 \
+        # libcudnn8-dev \
         build-essential \
         cmake \
         git \
